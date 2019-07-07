e76a5c5c3778be2f79c9d50d375454b913f931ee
Lindakat <lindakt16@gmail.com>
2019-07-07 01:29:44 -0500

    Adding post tag functionality

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,6 +1,9 @@
 <template>
   <Layout>
+      <h2> {{ $page.post.title }} </h2>
+      <span> {{$page.post.date }} </span>
     <div v-html="$page.post.content"/>
+    <div> {{ $page.post.tags }} </div>
   </Layout>
 </template>
 
@@ -9,6 +12,8 @@ query Post ($path: String!) {
   post: post (path: $path) {
     title
     content
+    tags
+    date
   }
 }
 </page-query>
